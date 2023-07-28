8f8e2ad2be807f937e1799128d161d32f0050ace
Kyle Gilliam <kgilliam125@gmail.com>
2023-07-27 23:23:57 -0700

    Initial program

---
+++ programs/speedrun-game-jam/src/lib.rs
@@ -0,0 +1,77 @@
+use anchor_lang::prelude::*;
+
+declare_id!("HGPJjX9fXH5dwJ8MkJTfmKbzUF7nLECjrB3ovts4DuBb");
+
+#[program]
+pub mod speedrun_game_jam {
+    use super::*;
+
+    pub fn init_game_state_for_player(ctx: Context<InitGameStateForPlayer>) -> Result<()> {
+        // todo check to see if initialized
+
+        let game_state = &mut ctx.accounts.game_state;
+
+        game_state.player = *ctx.accounts.signer.key;
+        game_state.ore = 0;
+        game_state.crystal = 0;
+        game_state.platinum = 0;
+        game_state.is_initialized = true;
+
+        Ok(())
+    }
+
+    pub fn update_game_state(
+        ctx: Context<UpdateGameState>,
+        ore: u16,
+        crystal: u16,
+        platinum: u16,
+    ) -> Result<()> {
+        // require!(ctx.accounts.game_state.is_initialized == true, Error::);
+
+        let game_state = &mut ctx.accounts.game_state;
+
+        game_state.ore += ore;
+        game_state.crystal += crystal;
+        game_state.platinum += platinum;
+
+        Ok(())
+    }
+}
+
+// game signs this when started a new ranking season
+#[derive(Accounts)]
+pub struct InitGameStateForPlayer<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer=signer,
+        seeds=["state".as_bytes(), signer.key().as_ref()],
+        bump,
+        // Just make it large enough for expansion
+        space = 1000,
+    )]
+    pub game_state: Account<'info, GameState>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+// #[instruction(ore: u16, crystal: u16, platinum: u16)]
+pub struct UpdateGameState<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        seeds=["state".as_bytes(), signer.key().as_ref()],
+        bump,
+    )]
+    pub game_state: Account<'info, GameState>,
+}
+
+#[account]
+pub struct GameState {
+    player: Pubkey, // the public key for this player
+    ore: u16,       // actual_score = score / 100
+    crystal: u16,
+    platinum: u16,
+    is_initialized: bool,
+}
---
+++ tests/speedrun-game-jam.test.ts
@@ -0,0 +1,47 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SpeedrunGameJam } from "../target/types/speedrun_game_jam";
+
+describe("speedrun-game-jam", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.SpeedrunGameJam as Program<SpeedrunGameJam>;
+
+  const [gameStatePda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("state"), provider.publicKey.toBuffer()],
+    program.programId
+  );
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods
+      .initGameStateForPlayer()
+      .accounts({ gameState: gameStatePda })
+      .rpc();
+
+    const gameState = await program.account.gameState.fetch(gameStatePda);
+
+    expect(gameState.player).toEqual(provider.publicKey);
+    expect(gameState.ore).toEqual(0);
+    expect(gameState.crystal).toEqual(0);
+    expect(gameState.platinum).toEqual(0);
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Can mine ore!", async () => {
+    const tx = await program.methods
+      .updateGameState(1, 1, 1)
+      .accounts({ gameState: gameStatePda })
+      .rpc();
+
+    const gameState = await program.account.gameState.fetch(gameStatePda);
+
+    expect(gameState.ore).toEqual(0);
+    expect(gameState.crystal).toEqual(0);
+    expect(gameState.platinum).toEqual(0);
+    console.log("Your transaction signature", tx);
+  })
+  
+});
