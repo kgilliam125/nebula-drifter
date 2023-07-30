502e83e9d06aed5334ecf7c98215e406c405c60d
Kyle Gilliam <kgilliam125@gmail.com>
2023-07-30 14:31:12 -0700

    Push webgl build?!

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true,
-    testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)" ]
-  };
